ff2dfc82917215bcf4f77f48449fac32617d200f
leanjobs <ailahimi2805@gmail.com>
2023-11-05 23:36:03 +0700

    web movies

--- src/app/sections/ListCarousel.jsx
+++ src/app/sections/ListCarousel.jsx
@@ -1,17 +1,22 @@
 "use client"
-
 import { Carousel } from 'flowbite-react';
 import Image from 'next/image';
+import gambar1 from "@/assets/gambar1.jpg";
+import gambar2 from "@/assets/gambar2.jpg";
+import gambar3 from "@/assets/gambar3.jpg";
+import gambar4 from "@/assets/gambar4.jpg";
+import gambar5 from "@/assets/gambar5.jpg";
+
 
 export default function MyCarousel() {
   return (
     <div className="max-w-[1700px] h-[780px] w-full m-auto py-16 px-4">
       <Carousel pauseOnHover>
-        <Image src='https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg' alt="..." />
-        <Image src='https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/rMvPXy8PUjj1o8o1pzgQbdNCsvj.jpg' alt="..." />
-        <Image src='https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/h56edmERPTkey89SqyKu4hINVNy.jpg' alt="..." />
-        <Image src='https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/h56edmERPTkey89SqyKu4hINVNy.jpg' alt="..." />
-        <Image src='https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/pA3vdhadJPxF5GA1uo8OPTiNQDT.jpg' alt="..." />
+        <Image src={gambar1} alt="..." />
+        <Image src={gambar2} alt="..." />
+        <Image src={gambar3} alt="..." />
+        <Image src={gambar4} alt="..." />
+        <Image src={gambar5} alt="..." />
       </Carousel>
     </div>
   );
